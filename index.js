0e5b96c9b79f6985c04171669edd0f763e13f919
isaacs <i@izs.me>
2014-01-20 14:13:27 -0800

    code

---
+++ index.js
@@ -0,0 +1,38 @@
+var README_MAXLEN = 64 * 1024
+
+module.exports = readmeTrim
+
+function readmeTrim(doc) {
+  var changed = false
+  var readme = doc.readme || ''
+  var readmeFilename = doc.readmeFilename || ''
+  if (doc['dist-tags'] && doc['dist-tags'].latest) {
+    var latest = doc.versions[doc['dist-tags'].latest]
+    if (latest && latest.readme) {
+      readme = latest.readme
+      readmeFilename = latest.readmeFilename || ''
+    }
+    for (var v in doc.versions) {
+      // If we still don't have one, just take the first one.
+      if (doc.versions[v].readme && !readme)
+        readme = doc.versions[v].readme
+      if (doc.versions[v].readmeFilename && !readmeFilename)
+        readmeFilename = doc.versions[v].readmeFilename
+
+      if (doc.versions[v].readme)
+        changed = true
+
+      delete doc.versions[v].readme
+      delete doc.versions[v].readmeFilename
+    }
+  }
+
+  if (readme && readme.length > README_MAXLEN) {
+    changed = true
+    readme = readme.slice(0, README_MAXLEN)
+  }
+  doc.readme = readme
+  doc.readmeFilename = readmeFilename
+
+  return changed
+}
